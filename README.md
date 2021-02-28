7948428889b48093431e2890c05ea27dbd6c241a
chiknas <nkuknas@gmail.com>
2021-02-28 19:45:23 +0000

    add command line arguments

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SwanCloudServerDeploy
+
+## swan-cloud-server-deploy.sh
+
+Deploys a fresh server to the system by removing and repulling the `chiknas/swancloud:latest` image. if there is already a deployed version with the container name `swancloud` it will shut it down and remove it before pulling the new image. 
+
+### Usage
+
+ Example command:
+ `./swan-cloud-server-deploy.sh -k "'key1'" -p /mnt/c/Users/username/Desktop/`
+
+ #### Command line arguments
+
+ * -k|--keys = (REQUIRED) api keys that the server will respond to. any unauthorized request is dropped. Keys should be passed in this form:
+ `-k "'key1','key2'"`
+
+ * -p|--path = (REQUIRED) the path in the host machine to mount to the container and use as storage for the uploaded files.
--- swan-cloud-server-deploy.sh
+++ swan-cloud-server-deploy.sh
@@ -1,22 +1,71 @@
 #!/bin/sh
-container="swancloud"
-apiKeys=
-hostPath=
+CONTAINER="swancloud"
+IMAGE="chiknas/swancloud:latest"
 
-# clear container
-docker stop $container
-docker rm $container
+# get command line arguments
+while [ $# -gt 0 ]
+do
+key="$1"
+
+case $key in
+    -k|--keys)
+    APIKEYS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--path)
+    HOSTPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+
+# Check required params and exit if not set
+if [ -z "$APIKEYS" ] ; then
+    echo "Must provide some known API keys to the environment with the -k|--keys flag." 1>&2
+    exit 1
+fi
+if [ -z "$HOSTPATH" ] ; then
+    echo "Must provide a host path to be used for storage with the -p|--path flag." 1>&2
+    exit 1
+fi
+
+# check docker container exists by trying to inspect it
+if docker inspect $CONTAINER ; then
+
+    # stop container if its running
+    if docker inspect --format '{{json .State.Running}}' $CONTAINER ; then
+        if docker stop $CONTAINER ; then
+            echo "Stopped container $CONTAINER"
+        else
+            echo "Unable to stop $CONTAINER. Exiting..."
+            exit 1
+        fi
+    fi
+
+    # remove container
+    if docker rm $CONTAINER ; then
+        echo "Removed container $CONTAINER"
+    else
+        echo "Unable to remove $CONTAINER. Exiting..."
+        exit 1
+    fi
+fi
 
 # refresh image
-docker pull chiknas/swancloud:latest
+docker image rm $IMAGE
+docker pull $IMAGE
+echo "Refreshed image $IMAGE"
 
 # deploy
-docker run \
--p 8080:8080 \
--v "${hostPath}":"/app/data" \
+echo "Starting deployment of $CONTAINER"
+docker run -d \
+-p 80:8080 \
+-v "${HOSTPATH}":"/app/data" \
 --env files.base-path=/app/data \
 --env spring.profiles.active=production \
---env security.api.keys="{'$apiKeys'}" \
+--env security.api.keys="{$APIKEYS}" \
 --env server.ssl.enabled=false \
---name $container \
-chiknas/swancloud:latest
+--name $CONTAINER \
+$IMAGE
