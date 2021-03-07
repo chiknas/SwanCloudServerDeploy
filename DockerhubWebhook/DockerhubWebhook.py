0cd2c71715c8cae32ae5f028de30d7937726b13f
chiknas <nkuknas@gmail.com>
2021-03-07 20:23:22 +0000

    add post endpoint to wire in dockerhub webhook

--- DockerhubWebhook/DockerhubWebhook.py
+++ DockerhubWebhook/DockerhubWebhook.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
+@app.route('/8e6fe373-c129-4ecf-97d9-95e36e8b1eac/trigger/call', methods = ['POST'])
+def handleNewSwanCloudImage():
+    return 'Hello World!'
 
-@app.route('/')
-def hello():
-    return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
