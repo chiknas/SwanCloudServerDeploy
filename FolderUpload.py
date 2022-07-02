ffc7fc1c5da9f4679462865622d70c93d4d6ce46
Nikos Kyknas <nkuknas@gmail.com>
2022-07-02 18:23:43 +0100

    update folder upload to handle files in nested folders

--- FolderUpload.py
+++ FolderUpload.py
@@ -1,5 +1,5 @@
 import requests
-from os import listdir
+import os
 import sys
 import time
 
@@ -10,14 +10,14 @@ passwd=""
 
 headers = {"Authorization": passwd}
 
-files = listdir(folder_path)
-
-for file in files:
-    binaryFile = open(folder_path + "/" + file, "rb")
-    response = requests.post(server, files={"data": binaryFile}, headers=headers)
-    if response.ok:
-        print("Uploaded file: " + file)
-    else:
-        sys.exit("Upload failed for file: " + file)
-    
-    time.sleep(0.3)
+for path, subdirs, files in os.walk(folder_path):
+    for name in files:
+        binaryFile = open(os.path.join(path, name), "rb")
+        response = requests.post(server, files={"data": binaryFile}, headers=headers)
+        if response.ok:
+            print("Uploaded file: " + name)
+        else:
+            print(response)
+            sys.exit("Upload failed for file: " + name)
+        
+        time.sleep(0.3)
